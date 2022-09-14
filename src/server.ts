aa89ff3b172762b371b6ec020a8989285c739eb0
Vandson Falcão <vandson.falcao@inbec.com.br>
2022-09-14 14:04:24 -0300

    feat: finished routes 2.0

--- src/server.ts
+++ src/server.ts
@@ -31,9 +31,6 @@ app.post("/game/:uuid/ads", async (req, res) => {
 		req.body;
 
 	const game = await prisma.game.findFirst({
-		select: {
-			id: true,
-		},
 		where: {
 			uuid,
 		},
@@ -61,13 +58,13 @@ app.post("/game/:uuid/ads", async (req, res) => {
 			yearsPlaying,
 			discord,
 			weekDays: weekDays.join(","),
-			hoursStart: convertHourStringToMinutes(hoursStart),
-			hoursEnd: convertHourStringToMinutes(hoursEnd),
+			hourStart: convertHourStringToMinutes(hoursStart),
+			hourEnd: convertHourStringToMinutes(hoursEnd),
 			useVoiceChannel,
 		},
 	});
 
-	return res.status(201).json(newAd);
+	return res.status(201).json({ ...newAd, id: undefined, gameId: undefined, gameUuid: game.uuid});
 });
 
 app.get("/game/:uuid/ads", async (req, res) => {
@@ -112,7 +109,7 @@ app.get("/game/:uuid/ads", async (req, res) => {
 	);
 });
 
-app.get("/ads/:uuid/discord", async (req, res) => {
+app.get("/ad/:uuid/discord", async (req, res) => {
 	const uuid = req.params.uuid;
 
 	const ad = await prisma.ad.findFirstOrThrow({
--- src/utils/convert-minutes-to-hour-string.ts
+++ src/utils/convert-minutes-to-hour-string.ts
@@ -3,5 +3,5 @@ export default function convertMinutesToHourString(minutesAmount: number) {
 
 	const minutes = minutesAmount % 60;
 
-	return `${hours}:${minutes}`;
+	return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`;
 }
